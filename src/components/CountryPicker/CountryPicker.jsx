d2e9f3b595dc0ebf7c969867e7bba64e0e996444
Antônio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-06-09 16:31:38 -0300

    barChar: created

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -5,7 +5,7 @@ import styles from '../CountryPicker/CountryPicker.module.css';
 
 import { fetchCountries } from '../../api';
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
   const [fetchedCountries, setFetchedCountries] = useState([]);
 
   useEffect(() => {
@@ -18,7 +18,7 @@ const CountryPicker = () => {
 
   return (
     <FormControl className={styles.formControl} >
-      <NativeSelect>
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
         <option value="global"> global </option>
         {fetchedCountries.map(( country, i ) => <option key={i} value={ country }> { country } </option>)}
       </NativeSelect>
